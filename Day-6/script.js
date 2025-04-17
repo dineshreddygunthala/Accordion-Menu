23afc2b24deb1cb3360a10c91cf235f847e9f757
“samson” <reddysamson94@gmail.com>
2025-04-17 16:35:40 +0530

    new change

--- Day-6/script.js
+++ Day-6/script.js
@@ -2,13 +2,15 @@ function fetchPokemon() {
     const name = document.getElementById('pokemonName').value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
   
-    fetch(url) 
-    .then(function(response) {
-      return response.text(); 
-    })
-    .then(function(data) {
-      console.log("📦 Raw Response:", data); 
-      document.getElementById('rawData').textContent = data; 
-    });
+    fetch(url)
+      .then(function(response) {
+        return response.text(); 
+      })
+      .then(raw => {
+        console.log("📦 Raw Response:", raw);
+        document.getElementById('rawData').textContent = raw;
   
-}
+
+      });
+  }
+  
